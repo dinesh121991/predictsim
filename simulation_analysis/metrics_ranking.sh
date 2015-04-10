98c35db34e1b4e03cc7ec8afcb6e6c084fbb76ec
Valentin Reis <valentin.reis@gmail.com>
2015-04-10 20:06:58 +0200

    typos analysis scripts

--- simulation_analysis/metrics_ranking.sh
+++ simulation_analysis/metrics_ranking.sh
@@ -1,8 +1,8 @@
 #in an experiments folder,
 rm sim_analysis/metrics_complete_ranked*
 ../../../simulation_analysis/metrics_ranking_avgbsld.R sim_analysis/metrics_complete -o sim_analysis/metrics_complete_ranked_avgbsld
-../../../simulation_analysis/metrics_ranking_rmss.R sim_analysis/metrics_complete -o sim_analysis/metrics_complete_ranked_RMSS
+../../../simulation_analysis/metrics_ranking_rmss.R sim_analysis/metrics_complete    -o sim_analysis/metrics_complete_ranked_RMSS
 ../../../simulation_analysis/metrics_ranking_rmsbsld.R sim_analysis/metrics_complete -o sim_analysis/metrics_complete_ranked_RMSBSLD
-column -s' ' -t < sim_analysis/metrics_complete_ranked_avgbsld | less -#2 -N -S
+column -s' ' -t < sim_analysis/metrics_complete_ranked_avgbsld    | less -#2 -N -S
 column -s' ' -t < sim_analysis/metrics_complete_ranked_RMSS       | less -#2 -N -S
 column -s' ' -t < sim_analysis/metrics_complete_ranked_RMSBSLD    | less -#2 -N -S
