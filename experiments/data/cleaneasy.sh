c84202c2de32873908835be125a79b0c6bf5ca46
Valentin Reis <valentin.reis@gmail.com>
2015-04-16 21:22:59 +0200

    analysis data

--- experiments/data/cleaneasy.sh
+++ experiments/data/cleaneasy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for f in cross_validation_ctcsp2 cross_validation_kthsp2 cross_validation_sdscblue cross_validation_ceacurie cross_validation_sdscsp2;
+for f in cross_validation_ctcsp2 cross_validation_kthsp2 cross_validation_sdscblue cross_validation_ceacurie cross_validation_sdscsp2 cross_validation_meta;
 
 do
   rm $f/*
